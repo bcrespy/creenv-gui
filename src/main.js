7e87bb971abe00dd12d76b57a4ca49c2402d2f13
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-08 11:19:52 +0200

    test environment

--- src/main.js
+++ src/main.js
@@ -1,11 +1,58 @@
-var Slider = require("../lib/index");
+var GUI = require('../lib/index');
 
 
-let slider = new Slider(20,50,2, 30);
-let slider2 = new Slider(0,1000,0.1, 10);
 
-document.body.appendChild(slider.dom);
-document.body.appendChild(slider2.dom);
+var test = new GUI();
 
+console.log(test);
+
+
+
+
+// usage 
+var config = {
+  param1: 13,
+  param2: "#17171717",
+  param3: true,
+  param4: "test"
+};
+
+var userControls = {
+  object: config, 
+  controls: [
+
+    [
+      "folder name",
+
+      {
+        // object: config,
+        property: "param1",
+        min: 0, max: 150, step: 30,
+        callback: val => { console.log("number updated: "+val); },
+        callbackFinished: val => { console.log("number update finished "+val); }
+      },
+
+      {
+        property: "param2", 
+        callback: val => { console.log("new rgba color: "+val); },
+        callbackFinished: val => { console.log("new rgba color: "+val); }
+      }
+    ],
+
+    [
+      "other folder",
+
+      {
+
+      }
+    ]
+  ]
+}
+
+
+
+// il faut une fonction 
+// GUI.printParams qui retourne 
+// { param1: 13, param2: "#1818181" ... }
 
 //https://www.npmjs.com/package/pickr-widget
